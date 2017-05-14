b6dfee91f43f62e77d9d6132d4364952b82ce2bd
Shinmera <shinmera@tymoon.eu>
2017-05-14 17:35:34 +0200

    Implement a custom echo filter example

--- test.lisp
+++ test.lisp
@@ -1,5 +1,6 @@
 (ql:quickload '(cl-mixed cl-out123 cl-mpg123))
 
+;;; Shorthand stuff to set up the audio system
 (defun call-with-out (function rate channels encoding)
   (let ((out  (cl-out123:connect (cl-out123:make-output NIL))))
     (format T "~&Playback device ~a / ~a" (cl-out123:driver out) (cl-out123:device out))
@@ -46,6 +47,7 @@
                 (- read played))))
     (/= 0 read)))
 
+;;; Test for the 3d audio space segment
 (defun test-space (mp3 &key (samples 500) (width 100) (height 50) (speed 0.001))
   (with-edge-setup (file out samplerate :pathname mp3 :samples samples)
     (let* ((source (cl-mixed:make-source (cl-mpg123:buffer file)
@@ -79,6 +81,7 @@
                    while (play file out mixer samples))
           (cl-mixed:end mixer))))))
 
+;;; Test for a simple tone generator
 (defun test-tone (tones &key (samples 50) (type :sine))
   (with-out (out)
     (multiple-value-bind (rate channels encoding) (cl-out123:playback-format out)
@@ -119,3 +122,58 @@
                         (cl-out123:play out buffer buffersize)
                         (when (<= (first tones) time)
                           (pop tones) (pop tones)))))))))
+
+;;; Test for a custom audio filter
+(defclass echo (cl-mixed:virtual)
+  ((buffer :initform NIL :accessor buffer)
+   (offset :initform 0 :accessor offset)
+   (delay :initarg :delay :initform 0.2 :accessor delay)
+   (falloff :initarg :falloff :initform 0.8 :accessor falloff)
+   (samplerate :initarg :samplerate :initform 44100 :accessor samplerate)))
+
+(defmethod cl-mixed:start ((echo echo))
+  (setf (buffer echo) (make-array (ceiling (* (delay echo) (samplerate echo)))
+                                  :element-type 'single-float
+                                  :initial-element 0.0s0)))
+
+(defmethod cl-mixed:mix (samples (echo echo))
+  (let ((out (cl-mixed:data (aref (cl-mixed:outputs echo) 0)))
+        (in (cl-mixed:data (aref (cl-mixed:inputs echo) 0)))
+        (buf (buffer echo))
+        (offset (offset echo))
+        (falloff (falloff echo)))
+    (declare (type cffi:foreign-pointer in out))
+    ;; Mix
+    (loop for i from 0 below samples
+          for sample = (cffi:mem-aref in :float i)
+          for echo = (aref buf offset)
+          do (setf (cffi:mem-aref out :float i) (+ sample echo))
+             (setf (aref buf offset) (* (+ sample echo) falloff))
+             (setf offset (mod (1+ offset) (length buf))))
+    (setf (offset echo) offset)))
+
+(defun test-echo (mp3 &key (samples 500) (delay 0.2) (falloff 0.8))
+  (with-edge-setup (file out samplerate :pathname mp3 :samples samples)
+    (let* ((source (cl-mixed:make-source (cl-mpg123:buffer file)
+                                         (cl-mpg123:buffer-size file)
+                                         (cl-mpg123:encoding file)
+                                         (cl-mpg123:channels file)
+                                         :alternating
+                                         samplerate))
+           (drain (cl-mixed:make-drain (cl-mpg123:buffer file)
+                                       (cl-mpg123:buffer-size file)
+                                       (cl-out123:encoding out)
+                                       (cl-out123:channels out)
+                                       :alternating
+                                       samplerate))
+           (echo (make-instance 'echo :samplerate samplerate :falloff falloff :delay delay))
+           (mixer (cl-mixed:make-mixer source echo drain)))
+      (cl-mixed:with-buffers samples (li ri lo ro)
+        (cl-mixed:connect source :left echo 0 li)
+        (setf (cl-mixed:output :right source) ri)
+        (cl-mixed:connect echo :left drain :left lo)
+        (cl-mixed:connect echo :right drain :right ro)
+        (cl-mixed:start mixer)
+        (unwind-protect
+             (loop while (play file out mixer samples))
+          (cl-mixed:end mixer))))))
