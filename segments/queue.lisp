24772474f73501b9c22411a1b027a2d76beebb0a
Shinmera <shinmera@tymoon.eu>
2018-10-07 22:18:46 +0200

    Allow customising ins and outs

--- segments/queue.lisp
+++ segments/queue.lisp
@@ -9,9 +9,11 @@
 (defclass queue (segment)
   ((segments :initform (make-array 0 :adjustable T :fill-pointer T) :reader segments)))
 
-(defmethod initialize-instance :after ((segment queue) &key)
+(defmethod initialize-instance :after ((segment queue) &key inputs outputs)
   (with-error-on-failure ()
-    (cl-mixed-cffi:make-segment-queue (handle segment))))
+    (cl-mixed-cffi:make-segment-queue (handle segment)))
+  (when inputs (setf (in-count segment) inputs))
+  (when outputs (setf (in-count segment) outputs)))
 
 (defun make-queue (&rest args)
   (apply #'make-instance 'queue args))
