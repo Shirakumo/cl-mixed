8f6dc8ddd65df27d8609037aa5e57066ae7f7d8c
iskamag <iskamagg@gmail.com>
2022-12-29 20:30:54 +0500

    fix mpt

--- extensions/mpt.lisp
+++ extensions/mpt.lisp
@@ -55,7 +55,7 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
 (defmethod mixed:end ((source source)))
 
 (defmethod mixed:seek-to-frame ((source source) position)
-  (mpt:set-position (module source) (truncate position (samplerate source))))
+  (mpt:set-position (module source) (float (truncate position (samplerate source)) 0d0)))
 
 (defmethod mixed:frame-count ((source source))
   (truncate (* (mpt:get-duration (module source)) (samplerate source))))
