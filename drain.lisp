80e3b9bc7be5473b378a17d013cdcf99d23b2b9f
Gleefre <varedif.a.s@gmail.com>
2023-05-13 14:26:47 +0200

    Move stream and dont-close fields to wav:file-drain.

--- drain.lisp
+++ drain.lisp
@@ -73,35 +73,8 @@
     (format stream "~a" (device drain))))
 
 (defclass file-drain (drain)
-  ((file :initform NIL :initarg :file :reader file)
-   (stream :initform NIL :initarg :stream :accessor stream)
-   (dont-close :initform NIL :initarg :dont-close :accessor dont-close-p)))
+  ((file :initform NIL :initarg :file :reader file)))
 
 (defmethod print-object ((drain file-drain) stream)
   (print-unreadable-object (drain stream :type T :identity T)
     (format stream "~a" (file drain))))
-
-(defmethod start :before ((drain file-drain))
-  (with-slots (file stream) drain
-    (setf stream
-          (or stream
-              (open file :direction :output
-                         :element-type '(unsigned-byte 8))))))
-
-(defmethod mix ((drain file-drain))
-  (with-buffer-tx (data start size (pack drain))
-    (when (< 0 size)
-      (write-sequence data (stream drain) :start start :end (+ start size))
-      (finish size))))
-
-(defmethod end :after ((drain file-drain))
-  (when (and (stream drain)
-             (not (dont-close-p drain)))
-    (close (stream drain))
-    (setf (stream drain) NIL)))
-
-(defmethod free :after ((drain file-drain))
-  (when (and (stream drain)
-             (not (dont-close-p drain)))
-    (close (stream drain) :abort T)
-    (setf (stream drain) NIL)))
