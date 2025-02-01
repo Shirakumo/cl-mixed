864802d8503ed7f73aebf74a8b4f83135f354195
Yukari Hafner <shinmera@tymoon.eu>
2025-02-01 23:39:54 +0100

    Implement drain mix in C (maybe)

--- static/pipewire-spa.c
+++ static/pipewire-spa.c
@@ -18,6 +18,7 @@ __asm__(".symver strlen,strlen@GLIBC_2.17");
 
 #include<spa/param/audio/format-utils.h>
 #include<pipewire/loop.h>
+#include<mixed.h>
 
 struct spa_pod *_spa_format_audio_raw_build(struct spa_pod_builder *builder, uint32_t id, struct spa_audio_info_raw *info){
   return spa_format_audio_raw_build(builder, id, info);
@@ -43,4 +44,31 @@ int _pw_loop_iterate(struct pw_loop *loop, int count){
   return pw_loop_iterate(loop, count);
 }
 
-// cc -shared -o pipewire-spa-amd64.so -I/usr/include/spa-0.2 -I/usr/include/pipewire-0.3 -O3 -fPIC pipewire-spa.c
+struct pw_drain_data{
+  struct mixed_pack *pack;
+  struct pw_stream *stream;
+};
+
+int _pw_drain_mix(struct mixed_segment *segment){
+  struct pw_drain_data *data = (struct pw_drain_data *)segment->data;
+  uint32_t octets = UINT32_MAX;
+  void *area;
+  mixed_pack_request_read(&area, &octets, data->pack);
+  uint16_t framesize = mixed_samplesize(pack->format)*pack->channels;
+  struct pw_buffer *buf = pw_stream_dequeue_buffer(data->stream);
+  if(!buf) return 1;
+  struct pw_data_buffer *b = buf->buffer;
+  struct spa_data *d = b->data;
+  if(!d->data) return 1;
+
+  struct spa_chunk *c = d->chunk;
+  octets = min(min(samples, d->max_size), buf->requested*framesize);
+  memcpy(d->data, area, octets);
+  c->offset = 0;
+  c->stride = framesize;
+  c->size = octets;
+  pw_stream_queue_buffer(data->stream, buf);
+  mixed_pack_finish_read(octets, data->pack);
+}
+
+// cc -shared -o pipewire-spa-amd64.so -I/usr/include/spa-0.2 -I/usr/include/pipewire-0.3 -I../lib/src -O3 -fPIC pipewire-spa.c
